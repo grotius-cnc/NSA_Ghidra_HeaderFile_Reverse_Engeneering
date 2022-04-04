4825aa3b1f47446a9b8d06f4de76ec91caa00235
grotius-cnc <44880102+grotius-cnc@users.noreply.github.com>
2022-04-03 21:59:39 -0400

    Add files via upload
    add ghidra comments

--- HackMeExe/HackMeRE.h
+++ HackMeExe/HackMeRE.h
@@ -7,14 +7,20 @@ class HackMe
 {
 public:
 
-    //! This one is only in the lib memory. So it's value must be retrieved by this reverse header.
+    //! This one can not by found in Ghidra by memory and or string search.
     // std::string myString;
 
     HackMe();
 
     void setstring(std::string theString);
+    //! Ghidra decompiled will show u :
+    //! void __thiscall HackMe::setstring(HackMe *this,basic_string aString)
 
     std::string getstring();
+    //! Ghidra decompiled will show u :
+    //! HackMe * __thiscall HackMe::getstring[abi:cxx11](HackMe *this)
 };
 
 #endif // HACKMERE_H
+
+
